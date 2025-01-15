154ea242d575319b261de8db338397142088cb64
Naveen Khangrawat <khangrawat.naveen@gmail.com>
2025-01-15 21:12:20 +0530

    Refactor PaymentSuccess component to conditionally render based on search parameters

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import { useSearchParams } from 'next/navigation';
+
 import React from 'react'
 import PaymentSuccess from './_components/PaymentSuccess';
 
 function Page() {
 
-    const searchParams = useSearchParams();
-
     return (
         <div>
-            {searchParams.has('success') && (
-                <PaymentSuccess />
-            )}
+            <PaymentSuccess />
         </div>
     )
 }
