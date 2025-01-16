f54d1f8376415b911434807f35d1ce99782a26ff
Naveen Khangrawat <khangrawat.naveen@gmail.com>
2025-01-16 12:29:20 +0530

    fixed app banner

--- src/app/(home)/_components/landingPage/LandingPage.tsx
+++ src/app/(home)/_components/landingPage/LandingPage.tsx
@@ -38,7 +38,7 @@ function LandingPage() {
                     <div className='w-[75%] md:blur-[50px] blur-[30px] rounded-full h-32 absolute bg-purple-800/50 -z-10 top-[-30px]' />
                     <div className="w-full max-w-[1250px] sm:w-[95%] rounded-xl p-4 ring-1 ring-gray-700 backdrop-blur-xl bg-white/5">
                         <div className='rounded-xl overflow-hidden'>
-                            <Image src={'/playcodehub.png'} alt='Application banner' width={1250} height={600} className='w-full h-full object-cover' />
+                            <Image src={'/playcodehub.png'} alt='Application banner' width={1200} height={600} className='w-full h-full object-cover' />
                         </div>
                     </div>
                     <div className="bottom-0 top-[50%] bg-gradient-to-t from-[#0C1220] left-0 right-0 absolute z-10"></div>
